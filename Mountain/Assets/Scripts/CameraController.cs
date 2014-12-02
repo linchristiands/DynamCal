6b6de18d36974c7ac2f5d2d6031434a891cf4c64
Tony LAM <lamtony1992@gmail.com>
2014-12-02 22:39:10 +0100

    up

---
+++ Mountain/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	// Variables
+	public  GameObject cameraTarget;	// Object to look at or follow
+	private Transform thisTransform;    // Camera Transform
+	public float camZ = -10; 			// Position Z de la caméra
+
+	// Use this for initialization
+	void Start () {
+		thisTransform = transform;
+		camZ = transform.position.z;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = cameraTarget.transform.position.x;
+		float y = cameraTarget.transform.position.y;
+
+		transform.position = new Vector3 (x, y, -100);
+	}
+}
---
+++ Mountain/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public KeyCode KEY_JUMP = KeyCode.Space;
+
+	private bool isGrounding = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+
+
+
+	/*void OnTriggerStay(Collider other)
+	{
+		Debug.Log ("Stay");
+		Debug.Log (other.tag);
+	}*/
+	void OnCollisionStay(Collision collisionInfo) {
+		//Debug.Log ("Stay");
+		//Debug.Log ( collisionInfo.gameObject.tag );
+		/*foreach (ContactPoint contact in collisionInfo.contacts) {
+			Debug.DrawRay(contact.point, contact.normal * 100, Color.white);
+		}*/
+	}
+
+	// 
+	void OnCollisionEnter(Collision c)
+	//void OnTriggerEnter(Collider other)
+	{
+
+
+		Debug.Log("collision");
+		if (c.gameObject.tag == "BonusStar") {
+			
+			Destroy(c.gameObject);
+			isGrounding = true;
+		}
+
+
+
+
+		//Destroy(other.gameObject);
+		//Destroy (gameObject);
+	}
+
+	void OnTriggerEnter(Collider c){
+		Debug.Log("collision");
+		if (c.gameObject.tag == "BonusStar") {
+
+			Destroy(c.gameObject);
+			isGrounding = true;
+		}
+	}
+
+	void OnCollisionExit(Collision collision)
+		//void OnTriggerEnter(Collider other)
+	{
+		Debug.Log (collision.gameObject.tag);
+		
+		if (collision.gameObject.tag == "Terrain") {
+			
+			isGrounding = false;
+		}
+		
+		//Destroy(other.gameObject);
+		//Destroy (gameObject);
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		//Debug.Log (other.tag);
+		//Debug.Log ("On exit");
+		if (other.tag == "Terrain") {
+			isGrounding = false;
+		}
+		
+		//Destroy(other.gameObject);
+		//Destroy (gameObject);
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+	 // Permet de savoir si on touche le sol
+
+		if (Input.GetKeyDown( KEY_JUMP ) )
+		{
+			Debug.Log ("Jump button");
+			if( isGrounding )
+			rigidbody.velocity += Vector3.up * 40;
+		}
+
+
+	}
+}
