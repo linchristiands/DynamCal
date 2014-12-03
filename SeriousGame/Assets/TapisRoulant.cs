365102511d3ddc165130ee53e214db8a51758106
Tony LAM <lamtony1992@gmail.com>
2014-12-03 15:39:36 +0100

    serious game

---
+++ SeriousGame/Assets/TapisRoulant.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TapisRoulant : MonoBehaviour {
+
+	public Rigidbody objet;
+	public Transform origin;
+	
+	void Start(){
+		StartCoroutine ("Generate");
+	}
+
+	private IEnumerator Generate(){
+
+		while (true) {
+			yield return new WaitForSeconds (5f);
+			Rigidbody instance;	
+			instance = Instantiate (objet, origin.position, origin.rotation) as Rigidbody;
+			instance.AddForce (Vector3.forward * 10);
+		}
+	}
+}
---
+++ SeriousGame/Assets/cube1.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cube1 : MonoBehaviour {
+
+
+	void OnTriggerEnter(Collider c){
+		if (c.tag == "finTapis") {
+			Destroy(gameObject);		
+		}
+	}
+
+}
