bacf9c87c54453461d0ec5d17f996d2b2745dd77
Tony LAM <lamtony1992@gmail.com>
2014-12-30 12:20:22 +0100

    graph test

--- CalDynam/src/com/example/fragment/GraphFragment.java
+++ CalDynam/src/com/example/fragment/GraphFragment.java
@@ -1,7 +1,13 @@
 package com.example.fragment;
 
 import com.example.caldynam.R;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.PointsGraphSeries;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,12 +15,33 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class GraphFragment extends Fragment {
+	
+	View rootView;
+	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
  
-        View rootView = inflater.inflate(R.layout.fragment_graph, container, false);
-         
+        rootView = inflater.inflate(R.layout.fragment_graph, container, false);
+        GraphView graph = (GraphView) rootView.findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                  new DataPoint(0, 1),
+                  new DataPoint(1, 5),
+                  new DataPoint(2, 3),
+                  new DataPoint(3, 2)
+        });
+        
+        LineGraphSeries<DataPoint> series2 = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(2, 2),
+                new DataPoint(8, 6)
+      });
+        series.setColor(Color.RED);
+        series.setThickness(2);
+        series2.setThickness(2);
+        series2.setColor(Color.BLUE);
+        graph.addSeries(series);
+        graph.addSeries(series2);
+        graph.setTitle("in/out Graph");
         return rootView;
     }
 }
